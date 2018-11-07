091263c1eae7ca9418d3ba5715dc580c42b92771
LPinto <34793298+LesliePinto89@users.noreply.github.com>
2018-11-07 23:18:09 +0000

    Update ArrayUpdate.java

--- src/Google Code Challenges/ArrayUpdate.java
+++ src/Google Code Challenges/ArrayUpdate.java
@@ -3,36 +3,49 @@ import java.io.IOException;
 public class ArrayUpdate {
 
 	/** Increments array number sequence by one and adjusts size as needed */
-	public static void main(String[] args) throws IOException {
-
-		int carriedOver = 1;
-		int[] originalArray = { 9, 9, 9 };
-
-		// Used as first array with no size change and second for size
-		int[] newOrginArray = new int[originalArray.length];
-
+	private static void testUpdateArray(int[] originalArr, int[] newArr, int carried) {
 		// Start from end of array and work back to 0 index
-		for (int i = originalArray.length - 1; i >= 0; i--) {
-			int sum = originalArray[i] + carriedOver;
+		for (int i = originalArr.length - 1; i >= 0; i--) {
+			int sum = originalArr[i] + carried;
 			if (sum == 10) {
-				carriedOver = 1;
+				carried = 1;
 
 				// End carried over 1 for rest of loop
 			} else {
-				carriedOver = 0;
-				// Update first instance by 1, add others without increment
-				newOrginArray[i] = sum % 10;
+				carried = 0;
+
+				// update first instance by 1, add others without increment
+				newArr[i] = sum % 10;
 			}
 		}
 
 		// If all numbers sum over 10, make bigger array, add 1 at index 0
-		if (carriedOver == 1) {
-			newOrginArray = new int[originalArray.length + 1];
-			newOrginArray[0] = carriedOver;
-		}
+		if (carried == 1) {
+			newArr = new int[originalArr.length + 1];
+			newArr[0] = carried;
 
-		for (int i = 0; i < newOrginArray.length; i++) {
-			System.out.print(newOrginArray[i]);
 		}
+		for (int i = 0; i < newArr.length; i++) {
+			System.out.print(newArr[i]);
+		}
+		System.out.println("");
+	}
+
+	private static void addUpdateArray() {
+		int carriedOver = 1;
+		int[] originalArray = { 9, 9, 9 };
+
+		// Used as array with size change
+		int[] newArr = new int[originalArray.length];
+		testUpdateArray(originalArray, newArr, carriedOver);
+
+		originalArray = new int[] { 1, 2, 5, 7 };
+		// Also used as array for no size change
+		newArr = new int[originalArray.length];
+		testUpdateArray(originalArray, newArr, carriedOver);
+	}
+
+	public static void main(String[] args) throws IOException {
+		addUpdateArray();
 	}
 }
