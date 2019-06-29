8c25ae73bb31b21b5d0f81d638172efd707d5ebc
LPinto <34793298+LesliePinto89@users.noreply.github.com>
2019-06-29 23:03:49 +0100

    Refactored code
    Added assert samples and made the code more modular

--- src/Palantir Code Challenges/Palantir challenge 1 - Solution.java
+++ src/Palantir Code Challenges/Palantir challenge 1 - Solution.java
@@ -1,5 +1,4 @@
 package palantir;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,20 +10,38 @@ public class PalantirBase {
 	 * Write a program that checks whether an integer is a palindrome. For example,
 	 * 121 is a palindrome, as well as 888. 678 is not a palindrome. Do not convert
 	 * the integer into a string.
+	 * 
+	 * Added assert test with different samples
 	 */
 
 	public static void main(String[] args) {
-		Integer sample = 12001;
-		int decrement = 1;
-		int digitLength = (int) (Math.log10(sample) + 1);
+		Integer sampleOne = 8898; // not palindrome
+		checkIfPalindrome(sampleOne);
+		assert checkIfPalindrome(sampleOne) : false; 
+		
+		Integer sampleTwo = 12021; // is palindrome
+		checkIfPalindrome(sampleTwo);
+		assert checkIfPalindrome(sampleTwo) : true; 
+		
+		Integer sampleThree = 212303212; // is palindrome
+		checkIfPalindrome(sampleThree);
+		assert checkIfPalindrome(sampleThree) : true;
+	}
+	
+	public static boolean checkIfPalindrome(int input) {
+		int original = input;
 		List<Integer> digits = new ArrayList<Integer>();
-
-		if (sample == 0) {
+		int decrement = 1;
+		int digitLength = (int) (Math.log10(input) + 1);
+        boolean isPalindrome = false;
+		if (input == 0) {
 			System.out.print("Integer not a palidrome");
-		} else {
-			while (sample > 0) {
-				digits.add(sample % 10);
-				sample /= 10;
+		}
+		
+		else {
+			while (input > 0) {
+				digits.add(input % 10);
+				input /= 10;
 			}
 			Collections.reverse(digits);
 
@@ -33,14 +50,17 @@ public class PalantirBase {
 				if (digits.get(i) == digits.get(digitLength - decrement)) {
 					decrement++;
 					if (i + 1 == digits.size() / 2) {
-						System.out.print("Integer is a palidrome");
+						isPalindrome = true;
+						System.out.println("Integer: "+original +" is a palindrome");
 						break;
 					}
 				} else {
-					System.out.print("Integer not a palidrome");
+					isPalindrome =false;
+					System.out.println("Integer: "+original+" is not a palindrome");
 					break;
 				}
 			}
 		}
+		return isPalindrome;
 	}
 }
