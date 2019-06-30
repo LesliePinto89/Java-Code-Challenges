40001d61a0580fa456d735e8515c471e778cd2f3
LPinto <34793298+LesliePinto89@users.noreply.github.com>
2019-06-30 10:20:59 +0100

    Finished task - Find the highest common factor
    Finished task - Find the highest common factor

--- src/Math Coding Projects/Greatest Common Factor of collection/Core.java
+++ src/Math Coding Projects/Greatest Common Factor of collection/Core.java
@@ -2,33 +2,125 @@ package amazon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Core {
 
-	public static int index = 1;
-	public static int currentLargestDem = 0;
-	public static Integer[][] holdFactors;
+	private static Integer[][] holdFactors;
 
 	public static void main(String[] args) {
-		int [] sample = new int [] {56,42};
-		holdFactors = new Integer[sample.length][];
-		gatherPrimaryFactors(sample);
+		List<Integer> sample = Arrays.asList(56, 98, 42, 84,70);
+		prepDataSample(sample);
+		assert prepDataSample(sample) == 14;
+
+		List<Integer> sampleTwo = Arrays.asList(175, 55, 225);
+		prepDataSample(sampleTwo);
+		assert prepDataSample(sampleTwo) == 5;
 		
-		int gcf = matchCommonFactors();
-		String values = Arrays.toString(sample).replace("[", " ").replace("]"," ").replace(",", " ");
-        System.out.println("The GCF from the values ("+values+") is: "+gcf);
-		//assert findLargestDenominator(sample) == (Integer) 9;
+		List<Integer> sampleThree = Arrays.asList(81,909,45,450);
+		prepDataSample(sampleThree);
+		assert prepDataSample(sampleThree) == 9;
 	}
 
-	public static void findFactors(int i, Integer[] currentFactors) {
-		holdFactors[i] = currentFactors;
+	/**
+	 * Central point of computing HCF.
+	 * 
+	 * @param List<Integer> currentSample : The current collection of values to find
+	 *                      their HCF
+	 * @return the found HCF
+	 */
+	public static int prepDataSample(List<Integer> currentSample) {
+		Collections.sort(currentSample); // Re-order to smallest values first
+
+		// Prepare entries of each value's prime factors
+		holdFactors = new Integer[currentSample.size()][];
+
+		// Make as Integer to easier allow conversion to string
+		Integer[] targetArray = currentSample.toArray(new Integer[currentSample.size()]);
+
+		// find all prime factors each number
+		oneToValueFactorsApproach(targetArray);
+		int highestCommonFactor = foundHighestCommonFactor();
+		String values = Arrays.toString(targetArray).replace("[", " ").replace("]", " ").replace(",", " ");
+		System.out.println("The Highest Common Factor from the values (" + values + ") is: " + highestCommonFactor);
+		return highestCommonFactor;
 	}
 
-	public static void gatherPrimaryFactors(int [] currentSample) {
+	/**
+	 * This approach to finding factors works like this: Loop an index 
+	 * from 1 to the given value, and if remainder of value from index is
+	 * 0, add index to value's respective array for later.
+	 * 
+	 * @param Integer[] sample : The current collection of values to find their HCF
+	 */
+	public static void oneToValueFactorsApproach(Integer[] sample) {
+		List<Integer> tempFactors;
+		int holdFactorsIndex = 0;
+		for (int value : sample) {
+			tempFactors = new ArrayList<Integer>();
+			for (int i = 1; i < value; i++) {
+				if (value % i == 0) {
+					tempFactors.add(i);
+				}
+			}
+			Integer[] tempFactorsArray = tempFactors.toArray(new Integer[tempFactors.size()]);
+			holdFactors[holdFactorsIndex] = tempFactorsArray;
+			holdFactorsIndex++;
+		}
+	}
+
+	/**
+	 * Against holdFactors[0]'s elements, compare against other arrays' values and
+	 * update HCF until end of holdFactors[0] length.
+	 * 
+	 * @return the found HCF
+	 */
+	public static int foundHighestCommonFactor() {
+		List<Integer> commonFactors = new ArrayList<Integer>();
+		int x = 0;
+		int highestCommonFactor = 0;
+
+		for (int i = 1; i < holdFactors.length; i++) {
+			if (x == holdFactors[0].length) {
+				break;
+			}
+			for (int j = 0; j < holdFactors[i].length; j++) {
+				
+				// True when current x in smallest array, i.e holdFactors[0]
+				// is found in next array in iteration.
+				if (holdFactors[0][x] == holdFactors[i][j]) {
+
+					// Found at least once in all arrays,
+					// if true then add to holdFactors array index, 
+					// leave this loop, reset i back to 0,
+					// and move to next value i.e. x
+					if (i + 1 == holdFactors.length) {
+						highestCommonFactor = holdFactors[0][x];
+						commonFactors.add(holdFactors[0][x]);
+						break;
+					}
+					i++;
+					j = -1;
+				} else if (j == holdFactors[i].length - 1) {
+					break;
+				}
+			}
+			// Not found a given factor in smallest array,
+			// move to next one
+			i = 0;
+			x++;
+			continue;
+		}
+		return highestCommonFactor;
+	}
+	
+	 /**Kept this method to show comparison of finding factors from 1 to number approach,
+	 * and divide value by increments (2,3, and 2 continually until value is 1) approach.*/
+	/*  public static void divideToFindFactorsApproach(Integer [] currentSample) {
 		int increment = 2;
 		List<Integer> tempFactors;
-		int index = 0;
+		int holdFactorsIndex = 0;
 		for (int a : currentSample) {
 			tempFactors = new ArrayList<Integer>();
 			while (a != 1) {
@@ -57,64 +149,10 @@ public class Core {
 					increment += 2;
 				}
 			}
-			increment =2;
-			Integer[] array = tempFactors.toArray(new Integer[tempFactors.size()]);
-			findFactors(index, array);
-			index++;
+			increment =2;	
+			Integer[] tempFactorsArray = tempFactors.toArray(new Integer[tempFactors.size()]);
+			holdFactors[holdFactorsIndex] = tempFactorsArray;
+			holdFactorsIndex++;	
 		}
-	}
-
-	public static int matchCommonFactors() {
-		List<Integer> commonFactors = new ArrayList<Integer>();
-		int x = 0;
-		int j = 0;
-		
-		for (int i = 1; i < holdFactors.length; i++) {
-			for (; j < holdFactors[i].length;) {
-
-				// Found in next array, do this till end
-				if (holdFactors[i][j] == holdFactors[0][x]) {
-
-//					// Check all arrays, so add and move to next value
-//					if (i + 1 == holdFactors.length) {
-//						commonFactors.add(holdFactors[0][x]);
-//						break;
-//					}
-					i++;
-					j = -1;
-				}
-			}
-			if (x == holdFactors[0].length - 1) {
-				break;
-			}
-			// Not found a given factor, so move to next one from
-			i = 0;
-			x++;
-			continue;
-		}
-		Integer[] array = commonFactors.toArray(new Integer[commonFactors.size()]);
-		return findLargestDenominator(array);
-	}
-
-	public static int findLargestDenominator(Integer [] commonFactors) {
-		int sum = 0;
-		int temp =0;
-		boolean start =false;
-		for(int i =0; i <commonFactors.length; i++) {
-			if(!start) {
-				if(commonFactors.length==1) {
-					sum = commonFactors[i];
-					break;
-				}
-				temp = commonFactors[i] * commonFactors[i+1];
-				sum += temp;
-				start=true;
-			}
-			else if (i+1 !=commonFactors.length){
-				sum *= commonFactors[i+1];}
-			}
-			
-		return sum;
-		}
-	
+	}*/
 }
